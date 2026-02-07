d14cccb0e3e5f3b3079b4da371036c56d54c245f
SHIKHA SINHA <shikha.work.sinha@gmail.com>
2026-02-08 00:12:08 +0530

    first push

--- page-landing.php
+++ page-landing.php
@@ -9,7 +9,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
+    <title><?php bloginfo('name'); ?> | Master AI Prompting</title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
